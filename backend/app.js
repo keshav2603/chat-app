463edb319c49888b7832ff99d2cdd805e6d5bcdf
keshav2603 <keshavsingla2603@gmail.com>
2024-08-29 11:02:09 +0530

    done

--- backend/app.js
+++ backend/app.js
@@ -2,12 +2,14 @@ import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import { app } from "./socket/socket.js"; 
+// import path from "path";
 // const app = express();
 // CORS configuration
 app.use(cors({
     origin: "http://localhost:5173",
     credentials: true
 }));
+// const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(cookieParser());
@@ -22,5 +24,9 @@ app.use("/api/auth", authRoutes);
 app.use("/api/message", messageRoutes);
 app.use("/api/user", userRoutes);
 
+// app.use(express.static(path.join(__dirname,"/frontend/dist")))
 
+// app.get("*",(req,res)=>{
+//     res.sendFile(path.join(__dirname, "/frontend/dist/index.html"))
+// })
 export {app};
--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,6 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
 import { server } from "./socket/socket.js";
-import { app } from "./app.js";
 
 // Load environment variables
 dotenv.config({
